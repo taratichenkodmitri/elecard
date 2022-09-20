fe246a54d965b8921cf990e6a232b48dc50f77e6
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-09-20 15:18:15 +0600

    feat: add header and footer

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = process.env.NODE_ENV === 'production';
@@ -28,13 +27,11 @@ const optimization = () => {
 
 const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
 
-const esLintPlugin = isDev => isDev ? [] : [new ESLintPlugin({extensions: ['js']})];
-
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './main.js'
+        main: './index.js'
     },
     optimization: optimization(),
     output: {
@@ -49,21 +46,17 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename:  filename('css')
         }),
-        ...esLintPlugin(),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev
     }
 }
