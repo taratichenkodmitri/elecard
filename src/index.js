a32aba432cbbda7d6492b9b5d2a629105654c689
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-09-23 14:12:11 +0700

    fix: remove redundant files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/main.css'
-import {App} from "./modules/app";
-
-let app = new App();
-await app.init();
-app.run();
-
-
-
--- src/modules/dateConverter.js
+++
@@ -1,9 +0,0 @@
-export class DateConverter {
-    constructor() {
-    }
-
-    static convertTimestamp(timestamp) {
-        let date = new Date(timestamp);
-        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}::${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    }
-}
